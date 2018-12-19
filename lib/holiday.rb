02bf31ef004f70e4cb9af86e666d06992825a2d4
Lauren Days <lmchambers0420@gmail.com>
2018-12-19 04:16:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-holiday_hash[holiday_name][season]
+holiday_hash[season][holiday_name]
 end
 
 def all_winter_holiday_supplies(holiday_hash)
