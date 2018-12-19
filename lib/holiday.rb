6ce7c1318598abe5b66de75f53449703e470e60f
Lauren Days <lmchambers0420@gmail.com>
2018-12-19 04:24:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
-  puts#{"season.capitalize"
+  puts #""
 end
 
 def all_holidays_with_bbq(holiday_hash)
